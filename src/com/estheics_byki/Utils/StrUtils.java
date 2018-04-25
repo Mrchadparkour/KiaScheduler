e346a4ea4a73dfe2d7a01838d9c3159417c2d58b
mrchadparkour <cplattkuhn@gmail.com>
2018-04-25 07:25:21 -0600

    Make Main.java because I accidentally deleted it on the last commit

--- src/com/estheics_byki/Utils/StrUtils.java
+++ src/com/estheics_byki/Utils/StrUtils.java
@@ -1,7 +1,7 @@
 package com.estheics_byki.Utils;
 
 public class StrUtils {
-    public final static String titleCase(String str) {
+    public static String titleCase(String str) {
         String[] strs = str.toLowerCase().split("");
         strs[0] = strs[0].toUpperCase();
         return String.join("", strs);
